47dcb97901a463c4d9b90b77d3d218b8c3adb9f3
Mart7anova <mart7anova7@gmail.com>
2022-04-29 16:23:12 +0300

    created 1 test

---
+++ src/01/01-hello-tests/01.test.ts
@@ -0,0 +1,35 @@
+import {mult, sum} from './01';
+
+let a: number
+let b: number
+let c: number
+
+beforeEach(() => {
+    a = 1
+    b = 2
+    c = 3
+})
+
+test('sum should be correct', () => {
+    //action
+    const result1 = sum(a, b)
+    const result2 = sum(a, c)
+
+    //expect result
+    expect(result1).toBe(3)
+    expect(result2).toBe(4)
+})
+test('mult should be correct', () => {
+    //data
+    const a = 1
+    const b = 2
+    const c = 3
+
+    //action
+    const result1 = mult(a, b)
+    const result2 = mult(a, c)
+
+    //expect result
+    expect(result1).toBe(2)
+    expect(result2).toBe(3)
+})
